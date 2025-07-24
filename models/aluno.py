c8faf1954c6ac433743cf8d9c70ad61662ee8d43
Miller Monteiro <mmillermonteiro@gmail.com>
2025-07-24 11:56:13 -0300

    refact: adapted version for model aluno.py to tkinter views

--- models/aluno.py
+++ models/aluno.py
@@ -1,7 +1,7 @@
 from db.conn import Conn
 
 class Aluno:
-    def __init__(self, nome_completo, cpf, data_nascimento, telefone=None, email=None, id=None):
+    def __init__(self, id=None, nome_completo=None, cpf=None, data_nascimento=None, telefone=None, email=None):
         self.id = id
         self.nome_completo = nome_completo
         self.cpf = cpf
@@ -9,104 +9,51 @@ class Aluno:
         self.telefone = telefone
         self.email = email
 
-    def inserir(self):
-        db = Conn()
-        conn = db.conectar()
-        if not conn:
-            return None
-
-        cur = conn.cursor()
-        try:
-            cur.execute("""
-                INSERT INTO academia.aluno (nome_completo, cpf, data_nascimento, telefone, email)
-                VALUES (%s, %s, %s, %s, %s) RETURNING id
-            """, (self.nome_completo, self.cpf, self.data_nascimento, self.telefone, self.email))
-            self.id = cur.fetchone()[0]
-            conn.commit()
-            print(f"[INFO] Aluno inserido com ID: {self.id}")
-        except Exception as e:
-            print(f"[ERRO] Falha ao inserir aluno: {e}")
-            conn.rollback()
-        finally:
-            cur.close()
-            db.desconectar()
-        return self.id
-
-    def atualizar(self):
-        if not self.id:
-            print("[ERRO] ID do aluno não definido para atualização.")
-            return
-
-        db = Conn()
-        conn = db.conectar()
-        cur = conn.cursor()
-        try:
-            cur.execute("""
-                UPDATE academia.aluno
-                SET nome_completo = %s, cpf = %s, data_nascimento = %s, telefone = %s, email = %s
-                WHERE id = %s
-            """, (self.nome_completo, self.cpf, self.data_nascimento, self.telefone, self.email, self.id))
-            conn.commit()
-            print("[INFO] Aluno atualizado com sucesso.")
-        except Exception as e:
-            print(f"[ERRO] Falha ao atualizar aluno: {e}")
-            conn.rollback()
-        finally:
-            cur.close()
-            db.desconectar()
-
-    def deletar(self):
-        if not self.id:
-            print("[ERRO] ID do aluno não definido para exclusão.")
-            return
-
-        db = Conn()
-        conn = db.conectar()
-        cur = conn.cursor()
-        try:
-            cur.execute("DELETE FROM academia.aluno WHERE id = %s", (self.id,))
-            conn.commit()
-            print(f"[INFO] Aluno ID {self.id} deletado.")
-        except Exception as e:
-            print(f"[ERRO] Falha ao deletar aluno: {e}")
-            conn.rollback()
-        finally:
-            cur.close()
-            db.desconectar()
+    @staticmethod
+    def inserir(nome_completo, cpf, data_nascimento, telefone, email):
+        conn = Conn().conectar()
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO academia.Aluno (nome_completo, cpf, data_nascimento, telefone, email)
+            VALUES (%s, %s, %s, %s, %s)
+        """, (nome_completo, cpf, data_nascimento, telefone, email))
+        conn.commit()
+        conn.close()
 
     @staticmethod
-    def consultar(id):
-        db = Conn()
-        conn = db.conectar()
-        cur = conn.cursor()
-        aluno = None
-        try:
-            cur.execute("SELECT id, nome_completo, cpf, data_nascimento, telefone, email FROM academia.aluno WHERE id = %s", (id,))
-            row = cur.fetchone()
-            if row:
-                aluno = Aluno(*row[1:], id=row[0])
-        except Exception as e:
-            print(f"[ERRO] Falha ao consultar aluno: {e}")
-        finally:
-            cur.close()
-            db.desconectar()
-        return aluno
+    def atualizar(id, nome_completo, cpf, data_nascimento, telefone, email):
+        conn = Conn().conectar()
+        cursor = conn.cursor()
+        cursor.execute("""
+            UPDATE academia.Aluno SET nome_completo=%s, cpf=%s, data_nascimento=%s, telefone=%s, email=%s
+            WHERE id=%s
+        """, (nome_completo, cpf, data_nascimento, telefone, email, id))
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def deletar(id):
+        conn = Conn().conectar()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM academia.Aluno WHERE id=%s", (id,))
+        conn.commit()
+        conn.close()
 
     @staticmethod
     def listar_todos():
-        db = Conn()
-        conn = db.conectar()
-        cur = conn.cursor()
-        alunos = []
-        try:
-            cur.execute("SELECT id, nome_completo, cpf, data_nascimento, telefone, email FROM academia.aluno")
-            rows = cur.fetchall()
-            for row in rows:
-                alunos.append(Aluno(*row[1:], id=row[0]))
-        except Exception as e:
-            print(f"[ERRO] Falha ao listar alunos: {e}")
-        finally:
-            cur.close()
-            db.desconectar()
+        conn = Conn().conectar()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM academia.Aluno")
+        alunos = cursor.fetchall()
+        conn.close()
         return alunos
-    
+
+    @staticmethod
+    def consultar_por_cpf(cpf):
+        conn = Conn().conectar()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM academia.Aluno WHERE cpf = %s", (cpf,))
+        aluno = cursor.fetchone()
+        conn.close()
+        return aluno
+
